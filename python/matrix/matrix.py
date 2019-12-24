a29ce9e079cf399008510d2b37a6bf53fab45475
zqkal <zqkal@yahoo.com>
2019-12-24 14:27:05 -0500

    make the code dry. removed duplicatd logic to constractor

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,18 +1,20 @@
 class Matrix:
     def __init__(self, matrix_string):
-        self.matrix_string = matrix_string
+        self.matrix_string = matrix_string.split('\n')
+        self.row_item = []
+        for row in self.matrix_string:
+            self.row_item.append(row.split(' '))
 
     def row(self, index):
-        rows = []  
-        for row in self.matrix_string.split('\n'):
-            item = row.split(' ')
-            item = [int(i) for i in item]
-            rows.append(item)
-        return rows[index -1]
+        return [int(i) for i in self.row_item[index - 1]]
 
     def column(self, index):
-        columns = []
-        for row in self.matrix_string.split('\n'):
-            item = row.split(' ')[index - 1]
-            columns.append(int(item))
-        return columns
+        return [int(self.row_item[i][index - 1]) for i in range(len(self.row_item))]
+    
+
+# c = Matrix("89 1903 3\n18 3 1\n9 4 800")
+# # c = Matrix("1 2")
+# # c = Matrix("1 2 3\n4 5 6\n7 8 9")
+# c = Matrix("1 2 3 4\n5 6 7 8\n9 8 7 6")
+# print('row: ', c.row(3))
+# print('column: ', c.column(4))
